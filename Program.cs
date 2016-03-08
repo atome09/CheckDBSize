16d38137467e963bb4b17c8dce14ebbcfc1e2be9
atome09 <axtome09@gmail.com>
2016-03-07 23:13:14 -0800

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CheckDBSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlcon = "Server=storesysdb01;Database=StoreSys; Integrated Security=SSPI";
+            string _sqlcon = sqlConn.Connection(sqlcon);
+            Console.WriteLine(_sqlcon);
+            string sqlQuery = "SELECT StoreId,Status,IPAddress FROM stores where status = 'Open'";
+            DataTable getSql = sqlConn.sqlQuery(sqlQuery);
+
+            string SqlTrunc = "TRUNCATE TABLE StoresDBSize";
+            sqlConn.ExecuteNonQuery(SqlTrunc);
+
+            foreach (DataRow row in getSql.Rows)
+            {
+                string storeDB = "";
+                string storeid = row["storeid"].ToString();
+                string ipAddress = row["IPAddress"].ToString();
+
+                storeDB = "Server=" + ipAddress + ";Database=xstore;User Id=2wow05;Password=$98q442x;Connection Timeout=60";
+
+                using (SqlConnection _storeDB = new SqlConnection())
+                {
+                    try
+                    {
+                        _storeDB.ConnectionString = storeDB;
+                        _storeDB.Open();
+
+                        string insertCmd = "";
+                        string DBcmd = "SELECT DB_NAME() AS Name," +
+                                           "(size * 8) / 1024 DBSizeMB," +
+                                           "b.LogSizeMB" +
+                                        " FROM sys.master_files a join" +
+                                                " ( SELECT DB_NAME() AS DatabaseName, database_id," +
+                                                       " Name AS LogFile," +
+                                                        "Physical_Name, (size * 8) / 1024 LogSizeMB" +
+                                                    " FROM sys.master_files" +
+                                                    " WHERE DB_NAME(database_id) = 'xstore' and Name like '%_log%' ) b" +
+                                                " on a.database_id = b.database_id" +
+                                        " WHERE DB_NAME(a.database_id) = 'xstore' and Name like '%_data%'";
+
+                        if (_storeDB.State == ConnectionState.Open)
+                        {
+                            using (SqlCommand sqlCmd = new SqlCommand(DBcmd, _storeDB))
+                            {
+                                sqlCmd.CommandType = CommandType.Text;
+                                SqlDataReader reader = sqlCmd.ExecuteReader();
+
+                                if (reader.HasRows)
+                                {
+                                    while (reader.Read())
+                                    {
+
+                                        string dbSize = reader["DBSizeMB"].ToString();
+                                        string logSize = reader["LogSizeMB"].ToString();
+                                        Console.WriteLine("Name: " + reader["name"]);
+                                        Console.WriteLine("Size: " + reader["DBSizeMB"]);
+
+                                        insertCmd = "INSERT INTO StoresDBSize (StoreId,IPAddress,DBSizeMB,LogSizeMB,updateDate) VALUES ( " + "'" + storeid + "'," + "'" + ipAddress + "'," + "'" + dbSize + "'," + "'" + logSize + "',"+ "'" +DateTime.Now +"')";
+                                        sqlConn.ExecuteNonQuery(insertCmd);
+
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                    catch(SqlException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        string insertError = "INSERT INTO StoresDBSize (StoreId,IPAddress,errorMsg,updateDate) VALUES ( " + "'" + storeid + "'," + "'" + ipAddress + "'," + "'" + e.Message + "'," + "'" + DateTime.Now + "')";
+                        sqlConn.ExecuteNonQuery(insertError);
+                    }
+
+                    _storeDB.Close();
+                    
+                }
+
+                                    
+            }
+            sqlConn.Terminate();
+            Console.WriteLine("Process Done");
+            //Console.Read();
+
+
+        }
+    }
+}
---
+++ sqlConn.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CheckDBSize
+{
+    class sqlConn
+    {
+        private static SqlConnection conn;
+        private static SqlCommand cmd;
+        private static SqlDataAdapter da;
+        private static DataSet ds;
+        private static SqlDataReader dr;
+
+        public static string Connection(string sqlconn)
+        {
+            try
+            {
+                //string sqlconn = "Data Source=storesysdb01;Initial Catalog=StoreSys Integrated Security=SSPI";
+                conn = new SqlConnection(sqlconn);
+                conn.Open();
+            }
+            catch (SqlException e)
+            {
+                return e.Message;
+
+
+            }
+
+            return conn.State.ToString();
+
+
+        }
+
+        public static void Terminate()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+
+
+        public static DataTable sqlQuery(String sql)
+        {
+            ds = new DataSet();
+            cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+
+        public static int ExecuteNonQuery(string sql)
+        {
+            try
+            {
+                int affec;
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Connection = conn;
+                affec = cmd.ExecuteNonQuery();
+                return affec;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+
+            return -1;
+
+        }
+
+
+
+    }
+}
